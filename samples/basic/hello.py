6d0c2b7e6f8598b76ba6600008c265223f72eaf5
jackiex <xjg@ncepu.edu.cn>
2020-02-25 14:04:25 +0800

    更新hello.py

--- samples/basic/hello.py
+++ samples/basic/hello.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#! /usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   hello.py
+@Time    :   2020/02/24 12:26:06
+@Author  :   jackiex 
+@Version :   1.0
+'''
 
-# print('Hello, world')
-# print("武汉，加油！")
-
-#多行输出，代码如何换行，位置如何换行
-print("抗疫期间，大家做好本职工作，\
-      \n做好分内的事情(学生在家搞好学习，教师上好课)，\
-      \n同样是为社会做贡献！")
+s1="中文输出："
+print('Hello, world')
+print(s1+"武汉，加油！")
