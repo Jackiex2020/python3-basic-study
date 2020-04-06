f144d97a08646193ececc50a40bccab663d975c7
oracle8i <xjg@ncepu.edu.cn>
2020-04-06 20:24:14 +0800

    修改

--- samples/regex/do_re3.py
+++ samples/regex/do_re3.py
@@ -8,6 +8,48 @@
 
 '''爬取给定网页上的图片'''
 
+import os
+import re
+from urllib import request
+#获取网页的html，与requests包一样的功能
+def getHtml(url):
+    response = request.Request(url)
+    page = request.urlopen(response)  
+    html = page.read() #urllib用read()读取html；requests包用text读取html
+    return html
+
+#获取图片对应的src属性代码
+def getImagesUrl(html):
+    html=html.decode('utf-8')    
+    #通过re-compile-findall二连函数操作来获取图片src属性对应的代码
+    src = r'//[^\s]*?\.jpg'     # 正则表达式匹配规则
+    imgre = re.compile(src)     # re.compile()，可以把正则表达式编译成一个正则表达式对象
+    imglist = re.findall(imgre, html)    # 读取html中包含imgre（正则表达式）的数据,imglist是包含了所有src元素的数组
+    return  imglist
+
+#用urlretrieve下载图片到给定的路径。图片命名为0/1/2...之类的名字
+def downloadImages(img_url_list,path):
+    #判断给定的路径是否存在,不存在则创建
+    if not os.path.exists(path):
+        os.mkdir(path)
+    x = 0
+    for url in img_url_list:
+        imageurl='https:'+url   # 分析目标网页发现,其图片src中的链接,没有https头
+        request.urlretrieve(imageurl, path+'\%s.jpg' %x)
+        x += 1
+        print('downloading:  https:'+url,'\n')
+
+if __name__=='__main__':
+    url="https://www.imooc.com/course/list"  # 指定目标网址
+    path=r"E:\flask\python3-basic-study.git\downloadimage"   # 设定目标路径,前面的r表示后面的字符串不转义
+
+    html = getHtml(url)
+    images_url=getImagesUrl(html)
+
+    downloadImages(images_url,path)
+    print('download success!')
+
+
 # import requests  # 用于获取网页
 # from bs4 import BeautifulSoup  # 用于分析网页
 # import re  # 用于在网页中搜索我们需要的关键字
@@ -38,39 +80,5 @@
 #     print(img_link,img_name,'\n')
 #     download(img_link,img_name)
 
-import re
-from urllib import request
-#获取网页的html，与requests包一样的功能
-def getHtml(url):
-    response = request.Request(url)
-    page = request.urlopen(response)  
-    html = page.read() #urllib用read()读取html；requests包用text读取html
-    return html
-
-#获取图片对应的src属性代码
-def getImagesUrl(html):
-    html=html.decode('utf-8')    
-    #通过re-compile-findall二连函数操作来获取图片src属性对应的代码
-    src = r'//[^\s]*?\.jpg'     #正则表达式匹配规则
-    imgre = re.compile(src)     #re.compile()，可以把正则表达式编译成一个正则表达式对象
-    imglist = re.findall(imgre, html)    # 读取html中包含imgre（正则表达式）的数据,imglist是包含了所有src元素的数组
-    return  imglist
-
-#用urlretrieve下载图片。图片命名为0/1/2...之类的名字
-def downloadImages(img_url_list):
-    x = 0
-    for url in img_url_list:
-        #注意，这里的文件路径，每段路径的首字母一定要大写！！小写会识别出错
-        imageurl='https:'+url
-        request.urlretrieve(imageurl, 'E:\\Test_img\\%s.jpg' %x)
-        x += 1
-        print('downloading:  https:'+url,'\n')
-
-if __name__=='__main__':
-    html = getHtml("https://www.imooc.com/course/list")
-    images_url=getImagesUrl(html)
-
-    downloadImages(images_url)
-    print('download success!')
 
 
