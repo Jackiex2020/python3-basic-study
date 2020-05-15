6e9952a56f71ffa2bd4d04797ef75be42798d2c1
jackiex <xjg@ncepu.edu.cn>
2020-05-15 17:39:29 +0800

    增加了web开发部分的范例

---
+++ samples/web/bbs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# file:bbs.py
+# author:jackiex
+# datetime:2020/5/15 16:34
+# software: PyCharm
+'''
+this is function  description 
+'''
+# import module your need
---
+++ samples/web/login.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# file:login.py
+# author:jackiex
+# datetime:2020/5/15 16:24
+# software: PyCharm
+'''
+this is function  description 
+'''
+# import module your need
