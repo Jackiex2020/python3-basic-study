d5f14cfee7cacb9d17a9c361854eb2eec52fb405
jackiex <xjg@ncepu.edu.cn>
2020-05-19 18:26:16 +0800

    增加前后端分离，调取接口数据的范例

---
+++ samples/web/vue-demo/data/data.js
@@ -0,0 +1,23 @@
+var data = [
+	{
+		title : '标题1',
+		content : '习近平总书记回信激励云南贡山独龙族群众继续团结奋斗创造美好生活',
+		time: '2019-04-13',
+		zuoze: '新华社',
+		url:[]
+	},
+	{
+		title : '标题2',
+		content : '习近平定下的建设核心 海南自贸区一年交出亮眼成绩单',
+		time: '2019-04-14',
+		zuoze: '央视网',
+		url:["./images/1.jpg"]
+	},
+	{
+		title : '标题3',
+		content : '习近平强军思想引领人民空军建设',
+		time: '2019-04-14',
+		zuoze: '央视网',
+		url:["./images/2.jpg","./images/3.jpg","./images/4.jpg"]
+	}
+]
